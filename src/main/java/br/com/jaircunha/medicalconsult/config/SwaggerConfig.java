e7055b2f203d1ec4d0f5eef288abdf6c9490e2cf
Jair Cunha <Jair@pbsenac.intra>
2024-10-08 13:14:47 -0300

    Enviando versão completa para o Github

--- src/main/java/br/com/jaircunha/medicalconsult/config/SwaggerConfig.java
+++ src/main/java/br/com/jaircunha/medicalconsult/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-        SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser().readLocation("src/main/resources/swsagger.yml", null, null);
+        SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser().readLocation("src/main/resources/swagger.yml", null, null);
         return swaggerParseResult.getOpenAPI();
     }
 }
