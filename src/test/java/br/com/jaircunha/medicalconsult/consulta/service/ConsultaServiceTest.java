d02a25777fb786c5901ca0ee731dcb1c8e0a97d5
Jair Cunha <Jair@pbsenac.intra>
2024-10-25 08:40:55 -0300

    Todos os testes estão prontos, enviando para o Github

--- src/test/java/br/com/jaircunha/medicalconsult/consulta/service/ConsultaServiceTest.java
+++ src/test/java/br/com/jaircunha/medicalconsult/consulta/service/ConsultaServiceTest.java
@@ -9,9 +9,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ConsultaServiceTest {
@@ -38,5 +42,53 @@ class ConsultaServiceTest {
                 () -> assertEquals("Alessandro", resultado.getProfissional())
         );
 
+    }
+
+    @Test
+    void listarConsultas(){
+        Consulta consulta1 = new Consulta();
+        Consulta consulta2 = new Consulta();
+        consulta1.setProfissional("Dr.Eneas");
+        consulta2.setProfissional("Dr.Livesey");
+
+        List<Consulta> consultas = new ArrayList<>();
+        consultas.add(consulta1);
+        consultas.add(consulta2);
+
+        when(consultaRepository.findAll()).thenReturn(consultas);
+
+        var resultado = consultaService.listarConsultas();
+
+        assertAll(
+                () -> assertNotNull(resultado),
+                () -> assertEquals(2, resultado.size()),
+                () -> assertEquals("Dr.Nerfario", resultado.get(0).getProfissional())
+        );
+    }
+
+    @Test
+    void buscarConsulta(){
+        Consulta consulta = new Consulta();
+        consulta.setProfissional("Dr.Fulvio");
+
+        when(consultaRepository.findById(consulta.getIdConsulta())).thenReturn(Optional.of(consulta));
+
+        var resultado = consultaService.buscarConsulta(consulta.getIdConsulta());
+
+        assertAll(
+                () -> assertNotNull(resultado),
+                () -> assertEquals("Dr.Paulo", consulta.getProfissional())
+        );
+    }
+
+    @Test
+    void deletarConsulta(){
+        Consulta consulta = new Consulta();
+
+        when(consultaRepository.findById(consulta.getIdConsulta())).thenReturn(Optional.of(consulta));
+
+        consultaService.deletarConsulta(consulta.getIdConsulta());
+
+        verify(consultaRepository, times(1)).deleteById(consulta.getIdConsulta());
     }
 }
